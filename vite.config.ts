79fb96210ab60a653ce62b3b717d78dc0d549390
vuongnguyen2112 <a5ntvuong@gmail.com>
2025-11-13 11:46:01 +0700

    update deployment setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/password-generator/', // Thay 'password-generator' bằng tên repo của bạn
+  base: '/generate-password/', //
 })
